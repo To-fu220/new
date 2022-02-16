77dd0a88de33185f8231996f0292142f0fb4ab8f
kiluto <kiluto220@gmail.com>
2022-02-17 01:30:32 +0900

    2/17 ヘッダー遷移先調整

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+var Menu_order = ['Top','About','News','Member','Contact'];
+
 $(function(){
-    $('#header').load('parts/header.html');
+    $('#header').load('parts/header.html');//ヘッダーの配置
+    if($('#header').attr('name') == 'otherheader'){
+        $('.nav_list li').eq(Menu_order.indexOf('About')).find('a').attr('href','./#about');
+        $('.nav_list li').eq(Menu_order.indexOf('News')).find('a').attr('href','./#mininews');
+        $('.nav_list li').eq(Menu_order.indexOf('Member')).find('a').attr('href','./#member');
+    }
 });
