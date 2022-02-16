cccd539ee8ca622178df40a9b49e369b9dc0e2a5
kiluto <kiluto220@gmail.com>
2022-02-17 01:43:46 +0900

    2/17 修正

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var Menu_order = ['Top','About','News','Member','Contact'];
 $(function(){
     $('#header').load('parts/header.html');//ヘッダーの配置
     if($('#header').attr('name') == 'otherheader'){
+        console.log("otherheader");
         $('.nav_list li').eq(Menu_order.indexOf('About')).find('a').attr('href','./#about');
         $('.nav_list li').eq(Menu_order.indexOf('News')).find('a').attr('href','./#mininews');
         $('.nav_list li').eq(Menu_order.indexOf('Member')).find('a').attr('href','./#member');
